396d051105547cdb6f8e2101fdbcbecc5b4a8e54
Joshua Kerr <kerrjoshua@gmail.com>
2023-03-17 19:15:28 +0900

    checkTask middleware seems to be working

--- api/task/task-middleware.js
+++ api/task/task-middleware.js
@@ -1,4 +1,18 @@
-exports.checkTask = async function (req, res, next)  {
-    console.log('check task')
-    next()
+exports.checkTask = async function (req, res, next) {
+    const { task_description, project_id } = req.body
+    if (!task_description ||
+        typeof task_description !== 'string' ||
+        !task_description.trim()) {
+        next({ status: 400, message: 'Tasks need a description' })
+    }
+    else if (
+        !project_id ||
+        typeof project_id !== 'number' ||
+        project_id < 1
+    ) {
+        next({status:400, message: 'Tasks need a project_id'})
+    }
+    else {
+        next()
+    }
 }
